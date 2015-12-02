11a30c530bab8775b12c5facae08d3751cbc7b3c
peterobbin <beijingwang@gmail.com>
2015-12-01 21:37:44 -0500

    timer added

--- mic_solenoid/mic_solenoid.ino
+++ mic_solenoid/mic_solenoid.ino
@@ -2,14 +2,61 @@
 #define micPin 0
 
 
-void setup(){
+long previousHitTime = 0;
+long previousWaitTime = 0;
+long hitInterval = 90;
+long waitInterval = 1000;
+unsigned long currentTime = 0;
+boolean hitting = false;
 
+
+
+void setup(){
+  pinMode(soloPin, OUTPUT);
+  Serial.begin(9600);
 
 }
 
 
 void loop(){
 
+  currentTime = millis();
+
+
+
+
+
+  // timer setup for hit 
+  // using delay() will not grant us multitasking 
+  if (currentTime - previousHitTime < hitInterval && hitting == true){
+    digitalWrite(soloPin, HIGH);
+    Serial.println("hit");
+    Serial.println(currentTime - previousHitTime);
+  }else{
+    hitting = false;
+    previousHitTime = currentTime;
+  }
+  
+  // timer setup for wait
+  if (currentTime - previousWaitTime < waitInterval && hitting == false){
+    digitalWrite(soloPin, LOW);
+    Serial.println("wait");
+    Serial.println(currentTime - previousWaitTime);
+  }else{
+    hitting = true;
+    previousWaitTime = currentTime;
+  }
+  
+  
+  
+  
+  delay(10);
+
+
+
+
 
 
 }
+
+
