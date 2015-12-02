815819e44258d0ff74ee6e1b5dbf3291beae80e6
peterobbin <beijingwang@gmail.com>
2015-12-01 21:54:24 -0500

    mic touch working

--- mic_solenoid/mic_solenoid.ino
+++ mic_solenoid/mic_solenoid.ino
@@ -9,10 +9,14 @@ long waitInterval = 1000;
 unsigned long currentTime = 0;
 boolean hitting = false;
 
+float volumeHighLimit = 390;
+float volumeLowLimit = 250;
+
 
 
 void setup(){
   pinMode(soloPin, OUTPUT);
+  pinMode(micPin, INPUT);
   Serial.begin(9600);
 
 }
@@ -22,16 +26,33 @@ void loop(){
 
   currentTime = millis();
 
+  if (trigger() == true){
+    solenoid();
+  }
+
+}
 
 
+boolean trigger(){
+  // getting volume
+  // volume is normally 300ish, when touched it will go higher than 350 or lower than 300
+  float volume = analogRead(micPin);
+  Serial.println(volume);
+  if (volume < volumeLowLimit || volume > volumeHighLimit){
+    return true;
+  }else{
+    return false;
+  }
+}
 
 
+void solenoid(){
   // timer setup for hit 
   // using delay() will not grant us multitasking 
   if (currentTime - previousHitTime < hitInterval && hitting == true){
     digitalWrite(soloPin, HIGH);
-    Serial.println("hit");
-    Serial.println(currentTime - previousHitTime);
+//    Serial.println("hit");
+//    Serial.println(currentTime - previousHitTime);
   }else{
     hitting = false;
     previousHitTime = currentTime;
@@ -40,23 +61,12 @@ void loop(){
   // timer setup for wait
   if (currentTime - previousWaitTime < waitInterval && hitting == false){
     digitalWrite(soloPin, LOW);
-    Serial.println("wait");
-    Serial.println(currentTime - previousWaitTime);
+//    Serial.println("wait");
+//    Serial.println(currentTime - previousWaitTime);
   }else{
     hitting = true;
     previousWaitTime = currentTime;
   }
-  
-  
-  
-  
-  delay(10);
-
-
-
-
-
-
 }
 
 
